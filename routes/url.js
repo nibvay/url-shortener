d1cfcde7e8722a250bbb59c3828964ea63dd1426
Ivy Fang <nibvay06@gmail.com>
2023-12-30 17:56:06 +0800

    feat: valid url string format

--- routes/url.js
+++ routes/url.js
@@ -7,15 +7,31 @@ const router = express.Router();
 
 const BASE_URL = "http://nibvay-url-shorten-project/";
 
-function validUrl(url) {
-  if (url.length > 0) return true;
-  return false;
+function isValidUrl(url) {
+  try {
+    return Boolean(new URL(url));
+  } catch (e) {
+    return false;
+  }
 }
+
+// function isUrlValid(str) {
+//   const pattern = new RegExp(
+//     '^(https?:\\/\\/)?' + // protocol
+//       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+//       '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR IP (v4) address
+//       '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+//       '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+//       '(\\#[-a-z\\d_]*)?$', // fragment locator
+//     'i'
+//   );
+//   return pattern.test(str);
+//  }
 
 router.post("/shorten", async (req, res, next) => {
   const { originUrl } = req.body;
   try {
-    if (!validUrl(originUrl)) throw new CustomError({ message: "invalid origin url", status: 400 });
+    if (!isValidUrl(originUrl)) throw new CustomError({ message: "invalid origin url", status: 400 });
     const url = await Url.findOne({ originUrl });
     if (url) {
       res.status(200).json({ urlId: url.shortUrl });
